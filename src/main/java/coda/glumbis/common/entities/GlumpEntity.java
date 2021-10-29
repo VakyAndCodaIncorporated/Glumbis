36c6fb4958efff221513bfd81aa60d4ced3db4e8
Coda <cody.koelliker@outlook.com>
2021-10-29 09:36:08 -0600

    Revert "Revert "fix it? maybe?""
    This reverts commit ff7e5311

--- src/main/java/coda/glumbis/common/entities/GlumpEntity.java
+++ src/main/java/coda/glumbis/common/entities/GlumpEntity.java
@@ -1,22 +1,25 @@
 package coda.glumbis.common.entities;
 
 import coda.glumbis.common.entities.ai.control.SmoothFlyingMoveControl;
+import coda.glumbis.common.entities.ai.goals.GlumpAttackGoal;
 import coda.glumbis.common.entities.ai.goals.GlumpGoToTargetGoal;
 import coda.glumbis.common.init.GlumbisSounds;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.FlyingMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.FlyingAnimal;
+import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import software.bernie.geckolib3.core.IAnimatable;
@@ -29,21 +32,26 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 import javax.annotation.Nullable;
 
-public class GlumpEntity extends PathfinderMob implements IAnimatable, FlyingAnimal {
-    private static final EntityDataAccessor<Boolean> SOGGY = SynchedEntityData.defineId(GlumpEntity.class, EntityDataSerializers.BOOLEAN);
+public class GlumpEntity extends Monster implements IAnimatable {
     private final AnimationFactory factory = new AnimationFactory(this);
+    private static final EntityDataAccessor<Boolean> EXPLODING = SynchedEntityData.defineId(GlumpEntity.class, EntityDataSerializers.BOOLEAN);
 
     public GlumpEntity(EntityType<? extends GlumpEntity> p_i48567_1_, Level p_i48567_2_) {
         super(p_i48567_1_, p_i48567_2_);
-        this.moveControl = new SmoothFlyingMoveControl(this, 90, true);
+    }
+
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(EXPLODING, false);
     }
 
     @Override
     protected void registerGoals() {
         super.registerGoals();
-        this.goalSelector.addGoal(1, new GlumpGoToTargetGoal(this));
         this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 6.0F));
         this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(1, new GlumpGoToTargetGoal(this));
+        this.goalSelector.addGoal(1, new GlumpAttackGoal(this));
         this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, true));
     }
 
@@ -51,23 +59,6 @@ public class GlumpEntity extends PathfinderMob implements IAnimatable, FlyingAni
         return createMobAttributes().add(Attributes.MAX_HEALTH, 8.0D).add(Attributes.MOVEMENT_SPEED, 0.265F).add(Attributes.FLYING_SPEED, 0.35F).add(Attributes.ATTACK_DAMAGE, 2.0F).add(Attributes.FOLLOW_RANGE, 32.0D);
     }
 
-    @Override
-    public boolean canBeLeashed(Player p_21418_) {
-        return false;
-    }
-
-    public void setSoggy(boolean isSoggy) {
-        this.entityData.set(SOGGY, isSoggy);
-    }
-
-    public boolean getSoggy() {
-        return this.entityData.get(SOGGY);
-    }
-
-    protected void defineSynchedData() {
-        super.defineSynchedData();
-        this.entityData.define(SOGGY, false);
-    }
 
     @Override
     public boolean causeFallDamage(float p_147187_, float p_147188_, DamageSource p_147189_) {
@@ -83,14 +74,26 @@ public class GlumpEntity extends PathfinderMob implements IAnimatable, FlyingAni
     public void tick() {
         super.tick();
         boolean damageTick = tickCount % 20 == 10;
-        boolean soundTick = tickCount % 50 == 10;
+        boolean soundTick = tickCount % 120 == 10;
+        if(tickCount < 10){
+            this.level.addParticle(ParticleTypes.POOF, this.getRandomX(0.5D), this.getRandomY() - 0.25D, this.getRandomZ(0.5D), 0, 0.08d, 0);
 
+        }
         if (level.isRainingAt(blockPosition())) {
             if (damageTick) {
                 hurt(DamageSource.DROWN, 1.0F);
             }
         }
-
+        if(this.level.isClientSide()){
+            if(this.getRandom().nextFloat() < 0.12f){
+                this.level.addParticle(ParticleTypes.ANGRY_VILLAGER, this.getRandomX(0.5D), this.getRandomY() - 0.25D, this.getRandomZ(0.5D), 0, 0.08d, 0);
+            }
+        }
+        if(getExploding()){
+            if(this.level.isClientSide()) {
+                this.level.addParticle(ParticleTypes.EXPLOSION, this.getRandomX(0.5D), this.getRandomY() - 0.25D, this.getRandomZ(0.5D), 0, 0.08d, 0);
+            }
+        }
         if (soundTick) {
             playSound(GlumbisSounds.GLUMP_FLY.get(), 0.4F, 1.0F);
         }
@@ -117,8 +120,11 @@ public class GlumpEntity extends PathfinderMob implements IAnimatable, FlyingAni
         data.addAnimationController(new AnimationController<>(this, "controller", 10, this::predicate));
     }
 
-    @Override
-    public boolean isFlying() {
-        return false;
+    public void setExploding(boolean exploding){
+        this.entityData.set(EXPLODING, exploding);
+    }
+
+    public boolean getExploding(){
+        return this.entityData.get(EXPLODING);
     }
 }
