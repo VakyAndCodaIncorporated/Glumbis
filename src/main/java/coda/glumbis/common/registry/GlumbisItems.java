86f81df7f360d7aefbe7515ccc606dcd65f393ba
Coda <cody.koelliker@outlook.com>
2022-02-28 20:00:01 -0700

    glllllllllllllumbo

--- src/main/java/coda/glumbis/common/registry/GlumbisItems.java
+++ src/main/java/coda/glumbis/common/registry/GlumbisItems.java
@@ -2,7 +2,11 @@ package coda.glumbis.common.registry;
 
 import coda.glumbis.Glumbis;
 import coda.glumbis.common.items.*;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BowlFoodItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemNameBlockItem;
 import net.minecraft.world.item.Rarity;
@@ -19,8 +23,8 @@ public class GlumbisItems {
     public static final RegistryObject<Item> SOGGY_SOCKS = ITEMS.register("soggy_socks", () -> new SoggySocksArmorItem(EquipmentSlot.FEET));
 
     public static final RegistryObject<Item> CAT_ESSENCE = ITEMS.register("cat_essence", () -> new CatEssenceItem(new Item.Properties().tab(Glumbis.GROUP).rarity(Rarity.EPIC)));
-
-    public static final RegistryObject<Item> GLUMP_CANNON = ITEMS.register("glump_cannon", () -> new GlumpCannonItem(new Item.Properties().tab(Glumbis.GROUP).stacksTo(1).durability(124)));
+    public static final RegistryObject<Item> GLUMBO = ITEMS.register("glumbo", () -> new BowlFoodItem(new Item.Properties().tab(Glumbis.GROUP).rarity(Rarity.RARE).food(new FoodProperties.Builder().saturationMod(1.0F).nutrition(12).effect(() -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 200, 0), 1.0F).effect(() -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, 300, 0), 1.0F).effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 400, 0), 1.0F).alwaysEat().build()).stacksTo(1)));
+    public static final RegistryObject<Item> GLUMP_CANNON = ITEMS.register("glump_cannon", () -> new GlumpCannonItem(new Item.Properties().rarity(Rarity.RARE).tab(Glumbis.GROUP).stacksTo(1).durability(124)));
     public static final RegistryObject<Item> ROCKET_PROPELLED_GLUMP = ITEMS.register("rocket_propelled_glump", () -> new Item(new Item.Properties().tab(Glumbis.GROUP).stacksTo(16)));
 
     public static final RegistryObject<Item> CATNIP_SEEDS = ITEMS.register("catnip_seeds", () -> new ItemNameBlockItem(GlumbisBlocks.CATNIP.get(), (new Item.Properties()).tab(Glumbis.GROUP)));
