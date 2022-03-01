6b47e53f6ee1e2e4a6817cb50001adf7d65df534
Coda <cody.koelliker@outlook.com>
2022-03-01 14:45:37 -0700

    lotta work on energizing things, mostly looks lol. completely unfunctional rn

--- src/main/java/coda/glumbis/common/CommonEvents.java
+++ src/main/java/coda/glumbis/common/CommonEvents.java
@@ -6,12 +6,16 @@ import coda.glumbis.common.registry.GlumbisEntities;
 import coda.glumbis.common.registry.GlumbisItems;
 import coda.glumbis.common.registry.GlumbisParticles;
 import lain.mods.cos.api.CosArmorAPI;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.CameraType;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.NonNullList;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -36,6 +40,7 @@ import net.minecraft.world.level.storage.loot.*;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.client.event.ClientPlayerChangeGameTypeEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.ProjectileImpactEvent;
@@ -101,7 +106,7 @@ public class CommonEvents {
                 List<ItemStack> items = cat.level.getServer().getLootTables().get(BuiltInLootTables.FISHING).getRandomItems(new LootContext.Builder((ServerLevel) cat.level).withRandom(cat.getRandom()).create(LootContextParamSets.EMPTY));
                 Containers.dropContents(cat.level, cat.blockPosition(), NonNullList.of(ItemStack.EMPTY, items.toArray(new ItemStack[0])));
                 cat.setDeltaMovement(0, 0, 0);
-           }
+            }
             if (cat.getRandom().nextFloat() > 0.33F && cat.getRandom().nextFloat() <= 0.66F) {
                 cat.setLying(true);
                 cat.setOrderedToSit(true);
@@ -143,24 +148,63 @@ public class CommonEvents {
         Player player = event.player;
         InteractionHand hand = event.player.getUsedItemHand();
         ItemStack stack = player.getItemInHand(hand);
+        Level level = player.level;
+
+        for (EquipmentSlot slot : EquipmentSlot.values()) {
+            ItemStack armor = player.getItemBySlot(slot);
+
+            if (slot.getType().equals(EquipmentSlot.Type.ARMOR)) {
+                CompoundTag tag = armor.getOrCreateTag();
+                tag.putInt("Energized", 100);
+
+                if (armor.getTag().get("Energized") != null && armor.getTag().getInt("Energized") > 0) {
+
+                    // todo - make this a translatable text component
+                    armor.setHoverName(new TextComponent("Energized ").append(armor.getItem().getName(armor)).withStyle(Style.EMPTY.withColor(0x9eb8ff).withItalic(false)));
+
+                    // todo - make the particles only render to others players & if the player is in first person. we cant get the camera from the player, so idk what to do
+                    //if (!Minecraft.getInstance().options.getCameraType().isFirstPerson())
+
+                    float armorAmount = player.getArmorCoverPercentage();
+                    float particleAmount = armorAmount * 2;
+
+                    if (armorAmount >= 0.25F) {
+                        if (player.tickCount % 40 == 0 && player.getRandom().nextFloat() < particleAmount) {
+                            int i = slot.getIndex();
+
+                            level.addParticle(GlumbisParticles.STATIC_LIGHTNING.get(), player.getRandomX(1), player.getY() + (i * 0.4) + 0.25, player.getRandomZ(1), 0, 0.05, 0);
+
+                            System.out.println(i);
+                            System.out.println("Armor Amount = " + armorAmount);
+                            System.out.println("Particle Amount = " + particleAmount);
+                            System.out.println("Did check pass? " + (player.getRandom().nextFloat() < particleAmount ? "Yes." : "No."));
+                        }
+                    }
+                }
+            }
+        }
 
         if (stack.is(Items.NETHERITE_SWORD)) {
             CompoundTag tag = stack.getOrCreateTag();
 
+            // todo - needs a (boolean) tag for if its energized AND the energized amount
+            // todo - also i think the energized amount should only go down when you attack / take damage (armor), because otherwise the item pops up and down when the tag changes
             tag.putInt("Energized", 100);
 
             if (stack.getTag().get("Energized") != null && stack.getTag().getInt("Energized") > 0) {
 
-                Level level = player.level;
+                // todo - make this a translatable text component
+                stack.setHoverName(new TextComponent("Energized ").append(stack.getItem().getName(stack)).withStyle(Style.EMPTY.withColor(0x9eb8ff).withItalic(false)));
 
-                // account for skin customizability
-                Vec3 pos = new Vec3(player.getMainArm() == HumanoidArm.LEFT ? 0.3 : -0.3, 0.95, 0.95).yRot(-player.yBodyRot * ((float) Math.PI / 180f)).add(player.getX(), player.getY(), player.getZ());
+                // account for skin customizability & camera mode
+                boolean camera = Minecraft.getInstance().options.getCameraType().isFirstPerson();
+                Vec3 pos = new Vec3(player.getMainArm() == HumanoidArm.LEFT ? 0.3 : -0.3, 0.95, camera ? 0.2 : 1.0).yRot(-player.yBodyRot * ((float) Math.PI / 180f)).add(player.getX(), player.getY(), player.getZ());
 
                 // todo - make the particles only render to others players & if the player is in first person. we cant get the camera from the player, so idk what to do
-                if (!Minecraft.getInstance().options.getCameraType().isFirstPerson()) {
+                //if (!Minecraft.getInstance().options.getCameraType().isFirstPerson())
+                if (player.tickCount % 40 == 0) {
                     level.addParticle(GlumbisParticles.STATIC_LIGHTNING.get(), pos.x(), pos.y(), pos.z(), 0, 0.05, 0);
                 }
-                System.out.println(pos);
             }
         }
     }
