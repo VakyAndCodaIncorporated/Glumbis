0a552288461b4b050639b1ec060901bd11632cb1
Cody <cody.koelliker@outlook.com>
2022-03-05 00:35:53 -0700

    added a single space

--- src/main/java/coda/glumbis/common/registry/GlumbisEntities.java
+++ src/main/java/coda/glumbis/common/registry/GlumbisEntities.java
@@ -17,7 +17,7 @@ public class GlumbisEntities {
     public static final RegistryObject<EntityType<GlumbossEntity>> GLUMBOSS = create("glumboss", EntityType.Builder.of(GlumbossEntity::new, MobCategory.CREATURE).sized(1.5f, 2.95f).fireImmune());
     public static final RegistryObject<EntityType<GlumpEntity>> GLUMP = create("glump", EntityType.Builder.of(GlumpEntity::new, MobCategory.CREATURE).sized(0.6f, 0.9f));
 
-    public static final RegistryObject<EntityType<RocketPropelledGlumpEntity>> ROCKET_PROPELLED_GLUMP = create("rocket_propelled_glump",EntityType.Builder.of(RocketPropelledGlumpEntity::new, MobCategory.MISC).sized(0.25f, 0.25f));
+    public static final RegistryObject<EntityType<RocketPropelledGlumpEntity>> ROCKET_PROPELLED_GLUMP = create("rocket_propelled_glump", EntityType.Builder.of(RocketPropelledGlumpEntity::new, MobCategory.MISC).sized(0.25f, 0.25f));
 
     private static <T extends Entity> RegistryObject<EntityType<T>> create(String name, EntityType.Builder<T> builder) {
         return ENTITIES.register(name, () -> builder.build(Glumbis.MOD_ID + "." + name));
