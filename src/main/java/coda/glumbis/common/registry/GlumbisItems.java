5b1f9853a45823da0afb5777756110944260e09c
Cody <cody.koelliker@outlook.com>
2022-11-09 01:43:22 -0700

    change temp spawn egg tab

--- src/main/java/coda/glumbis/common/registry/GlumbisItems.java
+++ src/main/java/coda/glumbis/common/registry/GlumbisItems.java
@@ -21,7 +21,7 @@ public class GlumbisItems {
     public static final RegistryObject<Item> SOGGY_SOCKS = ITEMS.register("soggy_socks", () -> new SoggySocksArmorItem(EquipmentSlot.FEET));
 
     // todo - remove spawn egg before release
-    public static final RegistryObject<Item> ROTTEN_EGG_SPAWN_EGG = ITEMS.register("rotten_egg_spawn_egg", () -> new ForgeSpawnEggItem(GlumbisEntities.GLUMBOSS, 0xfbfcfc, 0xfff167, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> ROTTEN_EGG_SPAWN_EGG = ITEMS.register("rotten_egg_spawn_egg", () -> new ForgeSpawnEggItem(GlumbisEntities.GLUMBOSS, 0xfbfcfc, 0xfff167, new Item.Properties().tab(Glumbis.GROUP)));
     public static final RegistryObject<Item> CAT_ESSENCE = ITEMS.register("cat_essence", () -> new CatEssenceItem(new Item.Properties().tab(Glumbis.GROUP).rarity(Rarity.EPIC)));
     public static final RegistryObject<Item> GLUMBO = ITEMS.register("glumbo", () -> new BowlFoodItem(new Item.Properties().tab(Glumbis.GROUP).rarity(Rarity.RARE).food(new FoodProperties.Builder().saturationMod(1.0F).nutrition(12).effect(() -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 200, 0), 1.0F).effect(() -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, 300, 0), 1.0F).effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 400, 0), 1.0F).alwaysEat().build()).stacksTo(16)));
     public static final RegistryObject<Item> GLUMP_CANNON = ITEMS.register("glump_cannon", () -> new GlumpCannonItem(new Item.Properties().rarity(Rarity.RARE).tab(Glumbis.GROUP).stacksTo(1).durability(124)));
