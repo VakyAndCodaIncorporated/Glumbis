0ca7d7b48e162891b55c083042615a3b7930d555
Cody <cody.koelliker@outlook.com>
2022-11-09 02:07:01 -0700

    bit more work on the glump coil

--- src/main/java/coda/glumbis/common/menu/GlumpCoilMenu.java
+++ src/main/java/coda/glumbis/common/menu/GlumpCoilMenu.java
@@ -6,8 +6,12 @@ import coda.glumbis.common.registry.GlumbisMenus;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.StackedContents;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.inventory.StackedContentsCompatible;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
 import java.util.Objects;
@@ -20,7 +24,70 @@ public class GlumpCoilMenu extends AbstractContainerMenu {
         super(GlumbisMenus.GLUMP_COIL.get(), windowId);
         this.glumpCoilBlockEntity = blockEntity;
         this.access = ContainerLevelAccess.create(Objects.requireNonNull(glumpCoilBlockEntity.getLevel()), glumpCoilBlockEntity.getBlockPos());
+
+        this.addSlot(new Slot(blockEntity, 0, 27, 47));
+        this.addSlot(new Slot(blockEntity, 0, 76, 47)); // todo - make this slot 'Cat Essence' only
+        this.addSlot(new Slot(blockEntity, 0, 134, 47)); // todo - make this a result slot only
+
+        for(int i = 0; i < 3; ++i) {
+            for(int j = 0; j < 9; ++j) {
+                this.addSlot(new Slot(blockEntity, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+            }
+        }
+
+        for(int k = 0; k < 9; ++k) {
+            this.addSlot(new Slot(blockEntity, k, 8 + k * 18, 142));
+        }
     }
+
+    public ItemStack quickMoveStack(Player p_38986_, int p_38987_) {
+        ItemStack itemstack = ItemStack.EMPTY;
+        Slot slot = this.slots.get(p_38987_);
+        if (slot != null && slot.hasItem()) {
+            ItemStack itemstack1 = slot.getItem();
+            itemstack = itemstack1.copy();
+            if (p_38987_ == 2) {
+                if (!this.moveItemStackTo(itemstack1, 3, 39, true)) {
+                    return ItemStack.EMPTY;
+                }
+
+                slot.onQuickCraft(itemstack1, itemstack);
+            } else if (p_38987_ != 1 && p_38987_ != 0) {
+/*                if (this.canSmelt(itemstack1)) {
+                    if (!this.moveItemStackTo(itemstack1, 0, 1, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (this.isFuel(itemstack1)) {
+                    if (!this.moveItemStackTo(itemstack1, 1, 2, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else */if (p_38987_ >= 3 && p_38987_ < 30) {
+                    if (!this.moveItemStackTo(itemstack1, 30, 39, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (p_38987_ >= 30 && p_38987_ < 39 && !this.moveItemStackTo(itemstack1, 3, 30, false)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (!this.moveItemStackTo(itemstack1, 3, 39, false)) {
+                return ItemStack.EMPTY;
+            }
+
+            if (itemstack1.isEmpty()) {
+                slot.set(ItemStack.EMPTY);
+            } else {
+                slot.setChanged();
+            }
+
+            if (itemstack1.getCount() == itemstack.getCount()) {
+                return ItemStack.EMPTY;
+            }
+
+            slot.onTake(p_38986_, itemstack1);
+        }
+
+        return itemstack;
+    }
+
 
     public GlumpCoilMenu(final int windowId, final Inventory playerInventory, final FriendlyByteBuf data) {
         this(windowId, playerInventory, getTileEntity(playerInventory, data));
